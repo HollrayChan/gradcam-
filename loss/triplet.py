75ec2db541a50b8140a89356575ec6bc21365779
HollrayChan <50159625+HollrayChan@users.noreply.github.com>
2020-07-11 15:29:25 +0800

    Update triplet.py

--- loss/triplet.py
+++ loss/triplet.py
@@ -90,75 +90,9 @@ class TripletLoss(nn.Module):
             dist_an.append(dist[i][mask[i] == 0].min().unsqueeze(0))
         dist_ap = torch.cat(dist_ap)
         dist_an = torch.cat(dist_an)
-        # print('dist_ap',dist_ap, 'dist_an', dist_an)
-        # print('cha', sum(dist_ap)-sum(dist_an))
         # Compute ranking hinge loss
         y = torch.ones_like(dist_an)
         loss = self.ranking_loss(dist_an, dist_ap, y)
         if self.mutual:
             return loss, dist
         return loss
-
-
-
-class DirectionOrientedTripletLoss(nn.Module):
-    """Triplet loss with hard positive/negative mining.
-
-    Reference:
-    Hermans et al. In Defense of the Triplet Loss for Person Re-Identification. arXiv:1703.07737.
-
-    Code imported from https://github.com/Cysu/open-reid/blob/master/reid/loss/triplet.py.
-
-    Args:
-        margin (float): margin for triplet.
-
-    edited by liuyw: add consideration for direction
-    """
-    def __init__(self, margin=0.3, mutual_flag = False):
-        super(DirectionOrientedTripletLoss, self).__init__()
-        self.margin = margin
-        self.ranking_loss = nn.MarginRankingLoss(margin=margin)
-        self.mutual = mutual_flag
-
-    def forward(self, inputs, targets, directions):
-        """
-        Args:
-            inputs: feature matrix with shape (batch_size, feat_dim)
-            targets: ground truth labels with shape (num_classes)
-        """
-        n = inputs.size(0)
-        #inputs = 1. * inputs / (torch.norm(inputs, 2, dim=-1, keepdim=True).expand_as(inputs) + 1e-12)
-        # Compute pairwise distance, replace by the official when merged
-        dist = torch.pow(inputs, 2).sum(dim=1, keepdim=True).expand(n, n)
-        dist = dist + dist.t()
-        dist.addmm_(1, -2, inputs, inputs.t())
-        dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
-        # For each anchor, find the hardest positive and negative
-        mask = targets.expand(n, n).eq(targets.expand(n, n).t())
-        mask_direction = directions.expand(n, n).eq(directions.expand(n, n).t())
-        #print(mask)
-        #print(mask_direction)
-        #print(mask[0] & mask_direction[0])
-        dist_ap, dist_an = [], []
-        for i in range(n):
-            aps = dist[i][(mask[i]) & mask_direction[i]]
-            ans = dist[i][(mask[i]==0) & mask_direction[i]]
-            if aps.shape[0] == 0 or ans.shape[0] == 0:
-                continue
-            dist_ap.append(aps.max().unsqueeze(0))
-            dist_an.append(ans.min().unsqueeze(0))
-        #print(dist_ap, dist_an)
-
-        if len(dist_ap) == 0:
-            return torch.FloatTensor([0]).type(dist.type())
-
-        dist_ap = torch.cat(dist_ap)
-        dist_an = torch.cat(dist_an)
-        # Compute ranking hinge loss
-        y = torch.ones_like(dist_an)
-        loss = self.ranking_loss(dist_an, dist_ap, y)
-        if self.mutual:
-            return loss, dist
-        return loss
-
-        
